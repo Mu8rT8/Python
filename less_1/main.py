db7c0f4b26a817ad454d3d087e54673823da353e
Maksim Kamarou <git.rep@mail.ru>
2023-04-09 14:06:39 +0200

    less 1

---
+++ less_1/main.py
@@ -0,0 +1,34 @@
+# c = 5.46
+# print(c)
+# print(type(c))
+# c = int(c)
+# print(c)
+# print(type(c))
+
+# c = 1
+# print(c)
+# print(type(c))
+# c = bool(c)
+# print(c)
+# print(type(c))
+
+# print('Введите первое число')
+# a = int(input())
+# b = int(input('Введите второе число'))
+# print(a, ' + ', b, ' = ', a + b )
+
+# a = 5.637
+# b = 12.75
+# print(round(a*b, 2))
+
+# a = 'qwerty'
+# for i in a:
+#     print(i)
+
+
+# line = ""
+# for i in range(5):
+#         line = ""
+#         for j in range(5):
+#             line += "*"
+#         print(line)
